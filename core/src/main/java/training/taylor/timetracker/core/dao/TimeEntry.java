0561fb92fb7765c3a9de9d1f2288eac8e6bcd0a3
steve <thompson4822@protonmail.com>
2018-08-12 12:10:38 -0600

    Added finals, as well as Javadoc

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -13,30 +13,58 @@ public class TimeEntry {
     private float rate;
     private int time;
 
+    /**
+     * What is the description?
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String value) {
+    /**
+     * Set the description to the given value
+     * @param value to set the description to
+     */
+    public void setDescription(final String value) {
         this.description = value;
     }
 
+    /**
+     * What is the rate?
+     * @return rate
+     */
     public float getRate() {
         return rate;
     }
 
-    public void setRate(float value) {
+    /**
+     * Set the rate to the given value
+     * @param value the rate
+     */
+    public void setRate(final float value) {
         this.rate = value;
     }
 
+    /**
+     * What is the time?
+     * @return the time
+     */
     public int getTime() {
         return time;
     }
 
-    public void setTime(int value) {
+    /**
+     * Set the time to the given value
+     * @param value the time
+     */
+    public void setTime(final int value) {
         this.time = value;
     }
 
+    /**
+     * What is the string representation of this instance?
+     * @return string representing the description, rate, and time
+     */
     @Override
     public String toString() {
         return "TimeEntry{" +
